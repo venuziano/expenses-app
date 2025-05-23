237f4cb72279c638dae87521a691d159220eedac
Rafael Rodrigues <raf4.rrs@gmail.com>
2025-05-23 17:02:27 -0300

    ADD: Add github actions to auto deploy

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -6,7 +6,6 @@ module.exports = {
       instances: 1,
       exec_mode: 'cluster', // or 'fork' if you prefer single instance
       max_memory_restart: '500M', // restart if >300 MB used
-      node_args: '-r dotenv/config',
       env_file: '.env'
     },
   ],
