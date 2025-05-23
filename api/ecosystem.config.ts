2e131b5f178d946954d8029de4b8a718e5f20cea
Rafael Rodrigues <raf4.rrs@gmail.com>
2025-05-23 09:11:01 -0300

    ADD: Add github actions to auto deploy

---
+++ api/ecosystem.config.ts
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'api',
+      script: './dist/main.js',
+      instances: 1,
+      exec_mode: 'cluster', // or 'fork' if you prefer single instance
+      max_memory_restart: '500M', // restart if >300 MB used
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
